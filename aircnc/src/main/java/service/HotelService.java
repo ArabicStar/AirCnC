23d69ac8fe36b4b280e9db25ee175a9e7dd77227
= <=>
2016-11-06 13:34:19 +0800

    hotel part 2

--- aircnc/src/main/java/service/HotelService.java
+++ aircnc/src/main/java/service/HotelService.java
@@ -1,7 +1,56 @@
 package service;
 
+import java.util.List;
+
+import vo.CommentVo;
 import vo.HotelVo;
+import vo.OrderVo;
 
 public interface HotelService {
+	
+	/**
+	 * 
+	 * @param hotelId
+	 * @return  获取酒店的最低房价
+	 */
+	public double getHotelLowestPrice(String hotelId);
+	
+	/**
+	 * 查看自己在该酒店的以往订单
+	 * @param userId
+	 * @param hotelId
+	 * @return  
+	 */
+	public List<OrderVo> checkMyOrder(String userId,String hotelId);
+	
+	
+	/**
+	 * 获取酒店详细信息
+	 * @param hotelId
+	 * @return
+	 */
+	public HotelVo getHotelDetailInfo(String hotelId);
+	
+	
+	/**
+	 * 获取酒店的简略信息，呈现在列表中
+	 * @param hotelId
+	 * @return
+	 */
+	public String getHotelSimpleInfo(String hotelId);
+	
+	
+	/**
+	 * 获取该酒店的评论
+	 * @param hotelId
+	 * @return
+	 */
+	public List<CommentVo> getHotelComments(String hotelId);
+	
+	
+	
+	
+	
+	
 	
 }
---
+++ aircnc/src/main/java/service/impl/HotelServiceImpl.java
@@ -0,0 +1,61 @@
+package service.impl;
+
+import java.util.List;
+
+import data.dao.HotelDao;
+import data.dao.OrderDao;
+import data.dao.impl.HotelDaoImpl;
+import data.dao.impl.OrderDaoImpl;
+import data.dao.impl.UserDaoImpl;
+import service.HotelService;
+import vo.CommentVo;
+import vo.HotelVo;
+import vo.OrderVo;
+
+public class HotelServiceImpl implements HotelService {
+	
+	private String hotelId;
+	private HotelDao hotelDao;
+	private OrderDao orderDao;
+	private List<CommentVo> commentList;
+	private List<OrderVo> orderList;
+
+	public HotelServiceImpl(String hotelId){
+		this.hotelId = hotelId;
+		hotelDao = HotelDaoImpl.getInstance();
+		orderDao = OrderDaoImpl.getInstance();
+//		orderList = orderDao.getOrders(hotelId);
+	}
+	
+	
+	@Override
+	public double getHotelLowestPrice(String hotelId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<OrderVo> checkMyOrder(String userId, String hotelId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HotelVo getHotelDetailInfo(String hotelId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHotelSimpleInfo(String hotelId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CommentVo> getHotelComments(String hotelId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
