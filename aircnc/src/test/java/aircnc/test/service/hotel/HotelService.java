4331f94c1185e7705a08d1250549fc8e5ddebac5
= <=>
2016-11-13 15:27:42 +0800

    hotel test

---
+++ aircnc/src/test/java/aircnc/test/service/hotel/HotelService.java
@@ -0,0 +1,16 @@
+package aircnc.test.service.hotel;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import service.impl.HotelServiceImpl;
+
+public class HotelService {
+	
+	@Test
+	public void HotelServiceTest1(){
+		
+	}
+
+}
