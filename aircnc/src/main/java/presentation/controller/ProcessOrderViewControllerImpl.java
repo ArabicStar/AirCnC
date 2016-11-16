77bec9a2ec3d0ca85a92d7a88b270c23edeb58ab
WanZ <598127577@qq.com>
2016-11-16 20:52:14 +0800

    已经拆分成功了

--- aircnc/src/main/java/presentation/controller/ProcessOrderViewControllerImpl.java
+++ aircnc/src/main/java/presentation/controller/ProcessOrderViewControllerImpl.java
@@ -4,85 +4,89 @@ import java.util.List;
 
 import presentation.view.ProcessOrderView;
 import presentation.view.ProcessOrderViewControllerService;
+import service.impl.order.OrderDetailServiceImpl;
+import service.impl.order.OrderListingServiceImpl;
+import service.impl.order.OrderLogicServiceImpl;
 import service.impl.order.OrderServiceImpl;
+import service.order.OrderDetailService;
+import service.order.OrderListingService;
+import service.order.OrderLogicService;
 import service.order.OrderService;
 import vo.order.OrderVo;
 
-public class ProcessOrderViewControllerImpl implements ProcessOrderViewControllerService{
-	
+public class ProcessOrderViewControllerImpl implements ProcessOrderViewControllerService {
+
 	private int hotelId;
-	
-	private OrderService orderService;
-	
-//	private UserService userService;
-	
+
+//	private OrderService orderService;
+	private OrderDetailService orderDetailService;
+	private OrderListingService orderListingService;
+	private OrderLogicService orderLogicService;
+
+	// private UserService userService;
+
 	private ProcessOrderView view;
-	
-	public ProcessOrderViewControllerImpl(int hotelId){
+
+	public ProcessOrderViewControllerImpl(int hotelId) {
 		this.hotelId = hotelId;
-		orderService = new OrderServiceImpl(hotelId);
-//		userService = new UserServiceImpl();
+//		orderService = new OrderServiceImpl(hotelId);
+		orderDetailService = new OrderDetailServiceImpl(hotelId);
+		orderListingService = new OrderListingServiceImpl(hotelId);
+		orderLogicService = new OrderLogicServiceImpl(hotelId);
+		// userService = new UserServiceImpl();
 	}
-	
-	public void setView(ProcessOrderView view){
+
+	public void setView(ProcessOrderView view) {
 		this.view = view;
 	}
-	
-	public int getHotelId(){
+
+	public int getHotelId() {
 		return hotelId;
 	}
-	
 
-	public List<OrderVo> getAllOrders(int hotelId){
-		return orderService.getAllOrders(hotelId);
+	public List<OrderVo> getAllOrders(int hotelId) {
+		return orderListingService.getAllOrders(hotelId);
 	}
-	
 
-	public List<OrderVo> getUnfinishedOrders(int hotelId){
-		return orderService.getUnfinishedOrders(hotelId);
+	public List<OrderVo> getUnfinishedOrders(int hotelId) {
+		return orderListingService.getUnfinishedOrders(hotelId);
 	}
-	
 
-	public List<OrderVo> getFinishedOrders(int hotelId){
-		return orderService.getFinishedOrders(hotelId);
+	public List<OrderVo> getFinishedOrders(int hotelId) {
+		return orderListingService.getFinishedOrders(hotelId);
 	}
-	
 
-	public List<OrderVo> getAbnormalOrders(int hotelId){
-		return orderService.getAbnormalOrders(hotelId);
+	public List<OrderVo> getAbnormalOrders(int hotelId) {
+		return orderListingService.getAbnormalOrders(hotelId);
 	}
-	
 
-	public boolean processUnfinishedOrder(int orderId){
-		if(orderService.finishOrder(orderId)){
-			int userId = orderService.getOrderUser(orderId);
-			int price = orderService.getOrderPrice(orderId);
-return false;
-			//return userService.addUserCredit(userId, price);
+	public boolean processUnfinishedOrder(int orderId) {
+		if (orderLogicService.finishOrder(orderId)) {
+			int userId = orderDetailService.getOrderUser(orderId);
+			int price = orderDetailService.getOrderPrice(orderId);
+			return false;
+			// return userService.addUserCredit(userId, price);
 		}
 		return false;
-		
-	}
-	
 
-	public boolean processAbnormalOrder(int orderId,String delayTime){
-		if(orderService.delayOrder(orderId, delayTime)){
-			int userId = orderService.getOrderUser(orderId);
-			int price = orderService.getOrderPrice(orderId);
-return false;
-			//return userService.addUserCredit(userId, price);
+	}
+
+	public boolean processAbnormalOrder(int orderId, String delayTime) {
+		if (orderLogicService.delayOrder(orderId, delayTime)) {
+			int userId = orderDetailService.getOrderUser(orderId);
+			int price = orderDetailService.getOrderPrice(orderId);
+			return false;
+			// return userService.addUserCredit(userId, price);
 		}
 		return false;
-		
-	}
-	
 
-	public void updateListModel(String comboboxValue){
+	}
+
+	public void updateListModel(String comboboxValue) {
 		view.updateListModel(comboboxValue);
 	}
-	
 
-	public void processOrderButtonClicked(){
+	public void processOrderButtonClicked() {
 		view.processOrderButtonClicked();
 	}
 
@@ -90,5 +94,4 @@ return false;
 		view.delayOrderButtonClicked();
 	}
 
-	
 }
