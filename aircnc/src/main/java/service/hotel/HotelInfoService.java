9a0400f4fe1f6fef24bcf359e4284fd0516f704e
= <=>
2016-11-16 21:00:45 +0800

    hotel info

--- aircnc/src/main/java/service/hotel/HotelInfoService.java
+++ aircnc/src/main/java/service/hotel/HotelInfoService.java
@@ -1,5 +1,19 @@
 package service.hotel;
 
-public class HotelInfoService {
+import java.util.List;
 
+import utils.info.hotel.HotelInfo;
+import vo.CommentVo;
+import vo.HotelVo;
+import vo.member.credit.CreditChangeVo;
+import vo.order.OrderVo;
+
+public interface HotelInfoService {
+	public HotelInfo getHotelInfo(String id);
+
+	public List<CommentVo> getHotelComment(String id);
+
+//	public List<PromotionVo> getHotelPromotion(String id);
+
+	public boolean updateInfo(HotelInfo modifiedInfo);
 }
---
+++ aircnc/src/main/java/utils/info/hotel/HotelInfoBuilder.java
@@ -0,0 +1,54 @@
+package utils.info.hotel;
+
+import utils.info.market.MarketInfoBuilder;
+
+public abstract class HotelInfoBuilder extends HotelInfoTemplate{
+	public HotelInfoBuilder(HotelInfo info){
+		this.setID(info.getID()).setName(info.getName()).setScope(info.getScope())
+		.setLocation(info.getLocaiton()).setStar(info.getStar()).setGrade(info.getGrade());
+	}
+	
+	protected HotelInfoBuilder(){
+		
+	}
+	
+	
+	public HotelInfoBuilder setID(String id) {
+		if (checkID(id))
+			this.id = id;
+		return this;
+	}
+
+	public abstract HotelInfoBuilder setName(String name);
+	
+	public HotelInfoBuilder setScope(String scope) {
+		if (checkHotelScope(scope))
+			this.scope = scope;
+		return this;
+	}
+	
+	public HotelInfoBuilder setLocation(String locaiton) {
+		if (checkHotelLocation(locaiton))
+			this.location = locaiton;
+		return this;
+	}
+	
+	public HotelInfoBuilder setIntro(String intro) {
+		if (checkHotelScope(intro))
+			this.introduction = intro;
+		return this;
+	}
+	
+	public HotelInfoBuilder setStar(int star) {
+		if (checkHotelStar(star))
+			this.star = star;
+		return this;
+	}	
+	
+	
+	public HotelInfoBuilder setGrade(Double grade) {
+		if (checkHotelGrade(grade))
+			this.grade = grade;
+		return this;
+	}
+}
