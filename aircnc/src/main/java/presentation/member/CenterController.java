10ec65c87b3a96810b53179f5e2c478f80b72ac9
VivianNJU <1916496828@qq.com>
2016-11-25 23:43:31 +0800

    photo

--- aircnc/src/main/java/presentation/member/CenterController.java
+++ aircnc/src/main/java/presentation/member/CenterController.java
@@ -14,40 +14,39 @@ import presentation.member.view.MemberMainPane;
 import presentation.member.view.signin.MemberRegisterEnterprisePane;
 
 /**
- * the whole stage.
- * contains the connection of every pane of sign in and register.
+ * the whole stage. contains the connection of every pane of sign in and
+ * register.
+ * 
  * @author paranoia
  *
  */
 
-public class CenterController extends Application{
-	
+public class CenterController extends Application {
+
 	private Stage primaryStage;
-	
+
 	private MemberStartPane start;
 	private MemberSignInPane signIn;
 	private MemberRegisterMainPane registerMain;
 	private MemberRegisterPersonPane registerPerson;
 	private MemberRegisterEnterprisePane registerBusiness;
-	
+
 	private MemberMainPane mainclient;
-	
+
 	private BorderPane rootLayout;
-	
+
 	private Scene scene;
-	
-	
+
 	private final static int Login_Width = 550;
 	private final static int Login_Height = 385;
-	
+
 	private final static int Client_Width = 1024;
 	private final static int Client_Height = 1024;
-	
 
 	public static void main(String[] args) throws Exception {
 		launch(args);
 	}
-	
+
 	/**
 	 * initialize the stage
 	 * 
@@ -55,67 +54,67 @@ public class CenterController extends Application{
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		this.primaryStage=primaryStage;
-		
+		this.primaryStage = primaryStage;
+
 		primaryStage.initStyle(StageStyle.UNDECORATED);
 		primaryStage.setTitle("AirCnC");
-        primaryStage.setResizable(false);
-        
-        //show the pane of sign in.
+		primaryStage.setResizable(false);
+
+		// show the pane of sign in.
 		start = new MemberStartPane(primaryStage);
 		rootLayout = start.getBorderPane();
-        scene = new Scene(start.getBorderPane(),Login_Width,Login_Height);
-        primaryStage.setScene(scene);
-        
-        addSignInPane();
-        
-        primaryStage.show();
+		scene = new Scene(start.getBorderPane(), Login_Width, Login_Height);
+		primaryStage.setScene(scene);
+
+		addSignInPane();
+
+		primaryStage.show();
 	}
-	
+
 	/**
 	 * add the pane of sign in (MemberSignInPane)
 	 */
-	public void addSignInPane(){
+	public void addSignInPane() {
 		start.getBorderPane().getChildren().clear();
 		signIn = new MemberSignInPane();
 		start.getBorderPane().setCenter(signIn.getPane());
 		signIn.getController().setCenterController(this);
 	}
-	
+
 	/**
 	 * add the pane of register (MemberRegisterMainPane)
 	 */
-	public void addRegisterPane(){
+	public void addRegisterPane() {
 		start.getBorderPane().getChildren().clear();
 		registerMain = new MemberRegisterMainPane();
 		registerMain.getController().setCenterController(this);
 		start.getBorderPane().setCenter(registerMain.getPane());
 	}
-	
+
 	/**
 	 * add the pane of personal register (MemberRegisterPersonPane)
 	 */
-	public void addRegisterPersonPane(){
+	public void addRegisterPersonPane() {
 		start.getBorderPane().getChildren().clear();
 		registerPerson = new MemberRegisterPersonPane();
 		registerPerson.getController().setCenterController(this);
 		start.getBorderPane().setCenter(registerPerson.getPane());
 	}
-	
+
 	/**
 	 * add the pane of business register (MemberRegisterEnterprisePane)
 	 */
-	public void addRegisterBusinessPane(){
+	public void addRegisterBusinessPane() {
 		start.getBorderPane().getChildren().clear();
 		registerBusiness = new MemberRegisterEnterprisePane();
 		registerBusiness.getController().setCenterController(this);
 		start.getBorderPane().setCenter(registerBusiness.getPane());
 	}
-	
+
 	/**
 	 * jump to the main client
 	 */
-	public void initializeClient(){
+	public void initializeClient() {
 		primaryStage.close();
 		ClientCenterController client = new ClientCenterController();
 		try {
--- aircnc/src/main/java/presentation/member/ClientCenterController.java
+++ aircnc/src/main/java/presentation/member/ClientCenterController.java
@@ -14,11 +14,12 @@ import presentation.member.view.creditchange.MemberCreditChangePane;
 
 /**
  * the controller of the whole client.
+ * 
  * @author paranoia
  *
  */
-public class ClientCenterController extends Application{
-	
+public class ClientCenterController extends Application {
+
 	private Stage primaryStage;
 	private Scene scene;
 	private MemberMainPane mainClient;
@@ -27,75 +28,76 @@ public class ClientCenterController extends Application{
 	private MemberCreditChangePane creditMain;
 	private MemberSearchHotelPane searchMain;
 	private MemberOrderMainPane orderMain;
-	
+
 	private BorderPane rootLayout;
-	
+
 	private final static int Client_Width = 1024;
 	private final static int Client_Height = 768;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		this.primaryStage=primaryStage;
-		
-		//primaryStage.initStyle(StageStyle.UNDECORATED);
+		this.primaryStage = primaryStage;
+
+		// primaryStage.initStyle(StageStyle.UNDECORATED);
 		primaryStage.setTitle("AirCnC");
-        primaryStage.setResizable(false);
-        
-        //show the pane of sign in.
+		primaryStage.setResizable(false);
+
+		// show the pane of sign in.
 		mainClient = new MemberMainPane(primaryStage);
 		mainClient.getController().setCenterController(this);
 		rootLayout = mainClient.getBorderPane();
-        scene = new Scene(mainClient.getBorderPane(),Client_Width,Client_Height);
-        primaryStage.setScene(scene);
-        
-        //addSignInPane();
-        
-        primaryStage.show();
-		
+		scene = new Scene(mainClient.getBorderPane(), Client_Width, Client_Height);
+		primaryStage.setScene(scene);
+
+		// addSignInPane();
+
+		primaryStage.show();
+
 	}
-	
-	public void addInfoMainPane(){
+
+	public void addInfoMainPane() {
 		clearContent();
 		infoMain = new MemberInfoMainPane();
 		mainClient.getBorderPane().setCenter(infoMain.getContentPane());
 		infoMain.getController().setCenterController(this);
 	}
-	
-	public void addInfoModifyPane(){
+
+	public void addInfoModifyPane() {
 		clearContent();
 		infoModify = new MemberInfoModifyPane();
 		mainClient.getBorderPane().setCenter(infoModify.getPane());
 		infoModify.getController().setCenterController(this);
 	}
-	
-	public void addCreditChangePane(){
+
+	public void addCreditChangePane() {
 		clearContent();
 		creditMain = new MemberCreditChangePane();
 		mainClient.getBorderPane().setCenter(creditMain.getPane());
 		creditMain.getController().setCenterController(this);
 	}
-	
-	public void addSearchHotelPane(){
+
+	public void addSearchHotelPane() {
 		clearContent();
 		searchMain = new MemberSearchHotelPane();
 		mainClient.getBorderPane().setCenter(searchMain.getPane());
 		searchMain.getController().setCenterController(this);
 	}
-	
-	public void addOrderMainPane(){
+
+	public void addOrderMainPane() {
 		clearContent();
 		orderMain = new MemberOrderMainPane();
 		mainClient.getBorderPane().setCenter(orderMain.getPane());
 		orderMain.getController().setCenterController(this);
 	}
-	
+
 	/**
-	 * remove all the children nodes of the main border pane, except the nav-bar.
+	 * remove all the children nodes of the main border pane, except the
+	 * nav-bar.
 	 */
-	public void clearContent(){
-		int childrenAmount=mainClient.getBorderPane().getChildren().size();
-		if(childrenAmount>1){
-			for(int i=1;i<childrenAmount;i++){
+	public void clearContent() {
+		int childrenAmount = mainClient.getBorderPane().getChildren().size();
+		if (childrenAmount > 1) {
+			for (int i = 1; i < childrenAmount; i++) {
 				mainClient.getBorderPane().getChildren().remove(i);
 			}
 		}
