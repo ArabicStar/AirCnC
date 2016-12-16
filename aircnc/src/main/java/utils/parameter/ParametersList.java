e3a69772196591784b855755e30c115f33f817db
ClefZz <tinker19981@hotmail.com>
2016-12-17 01:40:12 +0800

    little bug fix

--- aircnc/src/main/java/utils/parameter/ParametersList.java
+++ aircnc/src/main/java/utils/parameter/ParametersList.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
-
 import utils.lambda.SerializablePredicate;
 
 public class ParametersList {
