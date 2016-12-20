4607916a82b0c6937d0946abddc4f0457a266beb
ClefZz <tinker19981@hotmail.com>
2016-12-20 17:55:43 +0800

    little fix

--- aircnc/src/main/java/presentation/member/view/signin/fxml/MemberSignInController.java
+++ aircnc/src/main/java/presentation/member/view/signin/fxml/MemberSignInController.java
@@ -13,98 +13,87 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import presentation.member.CenterController;
 import presentation.member.accessor.MemberLoginAccessor;
+import presentation.member.accessor.impl.InfoModifyAccessorImpl;
 import presentation.member.accessor.impl.MemberLoginAccessorImpl;
 import presentation.member.manager.impl.MemberInfoManagerImpl;
 import presentation.member.utils.dialog.PlainDialog;
 
 /**
  * this is the controller of sign in pane.
+ * 
  * @author paranoia
  *
  */
 
-public class MemberSignInController implements Initializable{
+public class MemberSignInController implements Initializable {
 
 	@FXML
-    private TextField username;
+	private TextField username;
 
 	@FXML
-    private PasswordField password;
+	private PasswordField password;
 
 	@FXML
-    private Button confirm;
-	
+	private Button confirm;
+
 	@FXML
 	private Button register;
-	
+
 	private CenterController controller;
-	
+
 	private MemberLoginAccessor accessor;
 	private MemberAccountInteractor interactor;
-	
+
 	/**
-     * Initializes the controller class. This method is automatically called
-     * after the fxml file has been loaded.
-     */
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		username.setPromptText("用户名");
 		password.setPromptText("密码");
-		
+
 		accessor = MemberLoginAccessorImpl.getInstance();
 	}
-	
+
 	/**
-	 * handle the button action (Confirm)
-	 * check the input value
-	 * if the value is valid, jump to main client.
-	 * otherwise, pop up error message.
+	 * handle the button action (Confirm) check the input value if the value is
+	 * valid, jump to main client. otherwise, pop up error message.
 	 */
 	@FXML
-	public void handleConfirm(){
-		if(username.getText().length()!=0&&password.getText().length()!=0){
+	public void handleConfirm() {
+		if (username.getText().length() != 0 && password.getText().length() != 0) {
 			accessor.setDeliveredId(username.getText());
 			accessor.setDeliveredPassword(password.getText());
-			//下面是逻辑相连后的代码（没开服务器就会报错，暂时注释
+			// 下面是逻辑相连后的代码（没开服务器就会报错，暂时注释
 			interactor = MemberAccountCourier.getInstance();
-			interactor.login();
-			boolean valid = MemberInfoManagerImpl.getInstance() == null? false:true;
-			
-			if(valid)
-				controller.initializeClient();
-			else{
-				PlainDialog alert = new PlainDialog(AlertType.INFORMATION,
-						"登录失败","错误的用户名或密码！");
-				alert.showDialog();
-			}
-				
+			boolean valid = interactor.login();
 
-		}else{
-			PlainDialog alert = new PlainDialog(AlertType.INFORMATION,
-					"登录失败","请输入你的用户名和密码");
-			alert.showDialog();			
-		}		
-		
+			if (valid)
+				controller.initializeClient();
+		} else {
+			new PlainDialog(AlertType.INFORMATION, "登录失败", "请输入你的用户名和密码").showDialog();
+		}
+
 	}
-	
+
 	/**
-	 * handle the button action (Register)
-	 * jump to the register pane.
+	 * handle the button action (Register) jump to the register pane.
 	 */
 	@FXML
-	public void handleRegister(){
-		//注册跳转按钮，跳转至memberRegisterMainPane
+	public void handleRegister() {
+		// 注册跳转按钮，跳转至memberRegisterMainPane
 		controller.addRegisterPane();
-		
-	}
-	
-	/**
-	 * set the centerController
-	 * @param centerController
-	 */
-	public void setCenterController(CenterController controller){
-		this.controller=controller;
+
 	}
 
+	/**
+	 * set the centerController
+	 * 
+	 * @param centerController
+	 */
+	public void setCenterController(CenterController controller) {
+		this.controller = controller;
+	}
 
 }
