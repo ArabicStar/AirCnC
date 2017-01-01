273f767c6f3b2b63126f4851a10b437a080922f1
ClefZz <tinker19981@hotmail.com>
2017-01-01 13:13:27 +0800

    bug fix

--- aircnc/src/main/java/data/dao/order/OrderDao.java
+++ aircnc/src/main/java/data/dao/order/OrderDao.java
@@ -4,14 +4,44 @@ import po.order.OrderPo;
 
 public interface OrderDao {
 
+	/**
+	 * Get order by id.
+	 * 
+	 * @param orderId
+	 * @return order
+	 */
 	public OrderPo getOrder(String orderId);
 
+	/**
+	 * Update order.
+	 * 
+	 * @param orderPo
+	 * @return result of operation
+	 */
 	public boolean updateOrder(OrderPo orderPo);
 
+	/**
+	 * Add order.
+	 * 
+	 * @param orderPo
+	 * @return result of operation
+	 */
 	public boolean addOrder(OrderPo orderPo);
 
+	/**
+	 * Delete order by id.
+	 * 
+	 * @param orderId
+	 * @return result of operation
+	 */
 	public boolean deleteOrder(String orderId);
 
+	/**
+	 * Check given order id exists or not
+	 * 
+	 * @param orderId
+	 * @return existence
+	 */
 	public boolean existsOrder(String orderId);
 
 }
